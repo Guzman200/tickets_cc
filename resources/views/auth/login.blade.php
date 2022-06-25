33fd1e481db7ba18cbce30d4c9a44ce0bf420488
Pedro Guzman <my.rg.developer@gmail.com>
2022-06-24 23:25:32 -0500

    Se hicieron mejoras en el login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,7 +26,7 @@
                             <div class="card-body">
 
                                 @error('email')
-                                    <div class="alert alert-primary alert-dismissible fade show" role="alert">
+                                    <div style="color: white;" class="alert alert-primary alert-dismissible fade show" role="alert">
                                         <span class="alert-text">{{$message}}</span>
                                         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
                                             <span aria-hidden="true">&times;</span>
@@ -40,8 +40,7 @@
 
                                     <div class="input-group input-group-outline my-3">
                                         <label class="form-label">Correo Electronico</label>
-                                        <input name="email" type="email" class="form-control"
-                                            value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                        <input autocomplete="off" name="email" type="email" class="form-control" required autocomplete="email" autofocus>
                                     </div>
                                     <div class="input-group input-group-outline mb-3">
                                         <label class="form-label">Contraseña</label>
@@ -51,17 +50,12 @@
                                     <div class="form-check form-switch d-flex align-items-center mb-3">
                                         <input name="remember" class="form-check-input" type="checkbox"
                                             {{ old('remember') ? 'checked' : '' }}>
-                                        <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember me</label>
+                                        <label class="form-check-label mb-0 ms-2" for="rememberMe">Mantener sesión abierta</label>
                                     </div>
                                     <div class="text-center">
                                         <button type="submit"
                                             class="btn bg-gradient-primary w-100 my-4 mb-2">Ingresar</button>
                                     </div>
-                                    <p class="mt-4 text-sm text-center">
-                                        Don't have an account?
-                                        <a href="../pages/sign-up.html"
-                                            class="text-primary text-gradient font-weight-bold">Sign up</a>
-                                    </p>
                                 </form>
                             </div>
                         </div>
@@ -72,38 +66,15 @@
 
             <footer class="footer position-absolute bottom-2 py-2 w-100">
                 <div class="container">
-                    <div class="row align-items-center justify-content-lg-between">
-                        <div class="col-12 col-md-6 my-auto">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-12 col-md-2 my-auto">
                             <div class="copyright text-center text-sm text-white text-lg-start">
                                 © <script>
                                     document.write(new Date().getFullYear())
 
                                 </script>,
-                                made with <i class="fa fa-heart" aria-hidden="true"></i> by
-                                <a href="https://www.creative-tim.com" class="font-weight-bold text-white"
-                                    target="_blank">Creative Tim</a>
-                                for a better web.
+                                Naturafrut Bautista 
                             </div>
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com" class="nav-link text-white"
-                                        target="_blank">Creative Tim</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/presentation" class="nav-link text-white"
-                                        target="_blank">About Us</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/blog" class="nav-link text-white"
-                                        target="_blank">Blog</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-white"
-                                        target="_blank">License</a>
-                                </li>
-                            </ul>
                         </div>
                     </div>
                 </div>

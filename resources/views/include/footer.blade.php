8576a8cd3016b294e9c0eac1f75b93f03d842d5c
Pedro Guzman <my.rg.developer@gmail.com>
2022-10-30 23:00:31 -0600

    Cambio de texto en footer y color gris en login

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="col-lg-6 mb-lg-0 mb-4">
             <div class="copyright text-center text-sm text-muted text-lg-start">
             © {{date('Y')}},
-            Collecta Global
+            Collecta
             </div>
         </div>
         {{--

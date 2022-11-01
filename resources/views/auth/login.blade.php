87edc90896b76f16006c2613e94fcddc9ad3d0d9
Pedro Guzman <my.rg.developer@gmail.com>
2022-10-31 20:03:26 -0600

    Cambio fondo login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
 
     <main class="main-content  mt-0">
         <div class="page-header align-items-start min-vh-100"
-            style="background-image: url('{{asset('images/fondo_login.jpeg')}}');">
+            style="background-image: url('{{asset('images/fondo_login.jpg')}}');">
             <span class="mask bg-gradient-dark opacity-6"></span>
             <div class="container my-auto">
                 <div class="row">
                     <div class="col-lg-4 col-md-8 col-12 mx-auto">
                         <div class="card z-index-0 fadeIn3 fadeInBottom">
                             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                                <div class="bg-gradient-secondary shadow-primary border-radius-lg py-3 pe-1">
+                                <div class="bg-gradient-info shadow-info border-radius-lg py-3 pe-1">
                                     <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Ingresar</h4>
                                 </div>
                             </div>
@@ -54,7 +54,7 @@
                                     </div>
                                     <div class="text-center">
                                         <button type="submit"
-                                            class="btn bg-gradient-secondary w-100 my-4 mb-2">Ingresar</button>
+                                            class="btn bg-gradient-info w-100 my-4 mb-2">Ingresar</button>
                                     </div>
                                 </form>
                             </div>
